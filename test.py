bce59f077e96449cf079b9c17f528b529bcd16c1
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-02-18 17:36:53 -0500

    Python refresher

---
+++ test.py
@@ -0,0 +1,14 @@
+#lists
+nums = [1,2,3,4,5]
+ingredients = ['milk', 'eggs', 'butter', 'flour']
+
+for n in nums:
+	print(n)
+
+for i in ingredients:
+	print(i)
+
+greeting = 'hi'
+name = 'anthony'
+
+print(greeting + ' ' + name)
