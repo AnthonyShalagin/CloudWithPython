835875cd3a758370adee7c949f14b170c424c432
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-02-18 20:00:23 -0500

    Splitting and joining

--- slicingSplittingJoining.py
+++ slicingSplittingJoining.py
@@ -9,7 +9,7 @@ print(sentence[4]) #q
 print(colors[1]) #Orange
 print(colors[-2]) #Blue
 
-#slice 					[inclusive, exclusive] 
+##############  Slicing 					[inclusive, exclusive] ######################
 print(colors[1:4]) # [Orange, Yellow, Green]
 print(colors[::2]) #start at the beginning, go the end, take every other color
 
@@ -18,3 +18,13 @@ print(colors[0:4:2]) # [Red, yellow]
 #change the array of strings
 colors[0] = 'new color'
 print(colors[0])
+
+###########Splitting and joining								   ######################
+print(sentence.split(' '))
+
+coordinates = "12:34:65:74"
+print(coordinates.split(":"))
+
+print(" - ".join(colors)).split("-") #Splitting is the inverse of joining
+
+
